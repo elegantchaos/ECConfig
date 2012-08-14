35e2297824144385e5f425477a802154350f615b
Sam Deane <sam@elegantchaos.com>
2012-08-14 15:37:12 +0100

    copied in prefixes and configs from ECCore

---
+++ Generic/ECConfig.h
@@ -0,0 +1,13 @@
+//
+//  ECConfig.h
+//  ECConfig
+//
+//  Created by Sam Deane on 14/08/2012.
+//  Copyright (c) 2012 Elegant Chaos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ECConfig : NSObject
+
+@end
---
+++ Generic/ECConfig.m
@@ -0,0 +1,13 @@
+//
+//  ECConfig.m
+//  ECConfig
+//
+//  Created by Sam Deane on 14/08/2012.
+//  Copyright (c) 2012 Elegant Chaos. All rights reserved.
+//
+
+#import "ECConfig.h"
+
+@implementation ECConfig
+
+@end
