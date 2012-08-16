a340a54128d5fd7ef37ef6773a760923f10f4af8
Sam Deane <sam@elegantchaos.com>
2012-08-16 19:42:16 +0100

    updated documentation

--- Source/Generic/ECConfig.h
+++ Source/Generic/ECConfig.h
@@ -1,12 +1,20 @@
-//
-//  ECConfig.h
-//  ECConfig
-//
-//  Created by Sam Deane on 14/08/2012.
-//  Copyright (c) 2012 Elegant Chaos. All rights reserved.
-//
+// --------------------------------------------------------------------------
+//  Copyright 2012 Sam Deane, Elegant Chaos. All rights reserved.
+//  This source code is distributed under the terms of Elegant Chaos's
+//  liberal license: http://www.elegantchaos.com/license/liberal
+// --------------------------------------------------------------------------
 
 #import <Foundation/Foundation.h>
+
+/** Standard configurations and prefix files.
+ *
+ * The ECConfig framework doesn't actually contain any code at the moment.
+ *
+ * Instead, it's a standard place used to store:
+ *
+ * - standard xcconfig files
+ * - standard precompiled header files
+ */
 
 @interface ECConfig : NSObject
 
