88425aec1a6f805b76244d3b89db1f4519e053fe
Sam Deane <sam@elegantchaos.com>
2012-08-21 14:28:40 +0100

    tweaked grab script

--- Resources/grab-latest.sh
+++ Resources/grab-latest.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 
+check() {
+    if [[ $1 != 0 ]]; then
+      echo $2
+      exit $1
+    fi
+}
+    
 echo Fetching latest revisions
 git fetch
 
 echo Rebasing on origin/develop
-git rebase origin/develop
+git checkout -b develop-temp
+check $? "making temp branch"
+
+git rebase develop
+check $? "rebasing on develop"
+
+git rebase
+check $? "rebasing from origin"
+
+git checkout develop
+check $? "switching back to develop"
+
+git merge develop-temp
+check $? "merging changes from temp"
+
+git push
+check $? "pushing"
+
+git branch -d develop-temp
+check $? "removing temp branch"
 
