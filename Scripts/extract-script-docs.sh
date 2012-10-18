d8a0c80d65b1b772c34e890917a1c2fd84eaf4dd
Sam Deane <sam@elegantchaos.com>
2012-10-18 19:36:03 +0100

    updated extract script

--- Scripts/extract-script-docs.sh
+++ Scripts/extract-script-docs.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-file=$1
-comments=`grep "##" "$file"`
+## Extract any comments prefixed with "##" (like this one) in a file, and write them out to
+## a -template.md for inclusion in an appledoc documentation bundle.
 
-pattern="## ([a-z]*).git@github.com:([a-zA-Z0-9]+)\/"
-[[ $comments =~ $pattern ]]
-githubrepo=${BASH_REMATCH[1]}
-githubuser=${BASH_REMATCH[2]}
+output=$1
+shift
 
-base=`basename "$file"`
-name=${base%.*}
+while :; do
+	file=$1
+	if [[ "$file" == "" ]]; then
+		break
+	fi
+	
+	shift
+ 
+	comments=`grep "##" "$file"`
 
-echo "${comments//##/}" > "$name-template.md"
+	pattern="## ([a-z]*).git@github.com:([a-zA-Z0-9]+)\/"
+	[[ $comments =~ $pattern ]]
+	githubrepo=${BASH_REMATCH[1]}
+	githubuser=${BASH_REMATCH[2]}
+
+	base=`basename "$file"`
+	name=${base%.*}
+
+	echo "${comments//##/}" > "$output/$name-template.md"
+done
--- Scripts/merge-latest.sh
+++ Scripts/merge-latest.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+## This script is an attempt to automate picking up the latest version of the "develop" branch for a module, given that it might be on a detatch HEAD at the time.
+##
+## It performs the following steps:
+##
+## - rebase on the local develop branch
+## - save this to a temporary branch
+## - switch to the local develop branch
+## - merge in the temporary branch - this should be a fast forward
+## - remove the temporary branch
+## - rebase on the remote "develop" from origin
+## - push the resulting changed branch back to origin
+
 check() {
     if [[ $1 != 0 ]]; then
       echo "failed: $2"
